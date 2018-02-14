175fab3b313edaa210f8ca93f4ee4f2e0aec6ae3
kuma <hisakuma95@gmail.com>
2018-02-14 20:05:21 -0300

    datepicker
    calendario

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,15 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require twitter/bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
 //= require adminlte
+//= require bootstrap-datepicker
+//= require bootstrap-datepicker/core
+//= require bootstrap-datepicker/locales/bootstrap-datepicker.es.js
+//= require select2.full.js
+//= require select2.js
 //= require jquery.dataTables.min.js
 //= require dataTables.bootstrap.min.js
