2c52e88ad4b40cff951d3822102fa805af9cdd25
enzoM97 <31116070+enzoM97@users.noreply.github.com>
2018-10-16 09:49:50 -0300

    Delete data_users_test.rb

--- test/system/data_users_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class DataUsersTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit data_users_url
-  #
-  #   assert_selector "h1", text: "DataUser"
-  # end
-end
