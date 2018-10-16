1d3f40f6d8a89ec47db7bc99226242ff8ecbd8aa
enzoM97 <31116070+enzoM97@users.noreply.github.com>
2018-10-16 09:49:30 -0300

    Delete careers_test.rb

--- test/system/careers_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class CareersTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit careers_url
-  #
-  #   assert_selector "h1", text: "Career"
-  # end
-end
