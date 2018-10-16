f26afba4291b8a01787342d297db11fc590cae7d
enzoM97 <31116070+enzoM97@users.noreply.github.com>
2018-10-16 09:52:27 -0300

    Delete universitarios_test.rb

--- test/system/universitarios_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class UniversitariosTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit universitarios_url
-  #
-  #   assert_selector "h1", text: "Universitario"
-  # end
-end
