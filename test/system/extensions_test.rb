6d90060da52bad206d59d5ddbf96d8c869a3ca9e
enzoM97 <31116070+enzoM97@users.noreply.github.com>
2018-10-16 09:50:39 -0300

    Delete extensions_test.rb

--- test/system/extensions_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class ExtensionsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit extensions_url
-  #
-  #   assert_selector "h1", text: "Extension"
-  # end
-end
